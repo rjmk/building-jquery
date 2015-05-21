4c3cd58b6c3d4c232e9cdbf6fda1524bc4f35fcc
rjmk <rafekennedy@gmail.com>
2015-05-21 09:54:11 +0100

    some things

---
+++ jquery.js
@@ -0,0 +1,33 @@
+function $$(identifierString, parent){
+  var methodHolder = {};
+  methodHolder.style = function(attribute,value){
+    if (this.elements.length){
+      for (var i = 0; i < this.elements.length; i++){
+        this.elements[i].style[attribute] = value;
+      }
+    }
+    else this.elements.style[attribute] = value;
+
+    return this;
+  };
+
+  methodHolder.html = function(DOMString){
+    this.innerHTML = DOMString;
+  };
+
+  var that = Object.create(methodHolder);
+  var cleanIdentifier = identifierString.trim();
+  var identifierArray = cleanIdentifier.split(' ');
+  var target = parent || document;
+
+  if (cleanIdentifier[0] === '#'){
+    that.elements = [].slice.call(document.getElementById(identifierArray[0].slice(1)));
+  }
+  if (cleanIdentifier[0] === '.'){
+    that.elements = [].slice.call(target.getElementsByClassName(identifierArray[0].slice(1)));
+  }
+  else that.elements = target.getElementsByTagName(identifierArray[0]);
+
+  if (identifierArray.slice(1).length) that = $$(identifierArray.slice(1).join(' '),that);
+  return that;
+}
